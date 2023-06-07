787f7f064a4fb2873f9d038acb6836507044dddc
Pankaj Lohani <116501381+PlGehu@users.noreply.github.com>
2023-06-07 11:15:35 -0500

    Create SWAPPING_USING_FUNCTION_AND_POINTER.c

---
+++ Programs/SWAPPING_USING_FUNCTION_AND_POINTER.c
@@ -0,0 +1,24 @@
+int main() {
+    int num1, num2;
+    
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+    
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+    
+    printf("Before swapping - num1: %d, num2: %d\n", num1, num2);
+    
+  //FUNCTION DEFINE
+    swapNumbers(&num1, &num2);
+    
+    printf("After swapping - num1: %d, num2: %d\n", num1, num2);
+    
+    return 0;
+}
+
+void swapNumbers(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
