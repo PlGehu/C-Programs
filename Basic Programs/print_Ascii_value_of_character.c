8eb61821c1c357ed07def331472357d58b556ea7
Pankaj Lohani <116501381+PlGehu@users.noreply.github.com>
2023-06-12 19:40:08 -0500

    Create print_Ascii_value_of_character.c

---
+++ Basic Programs/print_Ascii_value_of_character.c
@@ -0,0 +1,19 @@
+//CODE:-
+
+#include <stdio.h>
+int main()
+{
+	char ch;
+	printf("Enter the character: ");
+	scanf("%c",&ch);
+
+	printf("ASCII is = %d", ch);
+	return 0;
+}
+
+//OUTPUT:-
+
+/*
+Enter the character: P
+ASCII is = 80
+*/
